28fe5cab4b1f7693f5252089aa6e9fe985481754
tbxark <tbxark@outlook.com>
2024-11-15 16:27:26 +0800

    fix: 导出模块错误

--- src/core/app/index.js
+++ src/core/app/index.js
@@ -1,4 +1,4 @@
-export default new Proxy(target, {
+export default new Proxy({}, {
     apply: function(target, thisArg, argumentsList) {
       console.log(`Function called with arguments: ${argumentsList.join(', ')}`);
     }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-export * as parsers from  '../Sub-Store/backend/src/core/proxy-utils/parsers/index.js'
-export * as produce from '../Sub-Store/backend/src/core/proxy-utils/producers/index.js'
+import parsersImport from  '../Sub-Store/backend/src/core/proxy-utils/parsers/index.js'
+import produceImport from '../Sub-Store/backend/src/core/proxy-utils/producers/index.js'
+
+export const parsers = parsersImport;
+export const produce = produceImport;
