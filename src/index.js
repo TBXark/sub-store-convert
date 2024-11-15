91ac09f58d8cfe9a3cef3c89e61ccf583b0fd895
tbxark <tbxark@outlook.com>
2024-11-15 16:19:45 +0800

    chore: 修改导出格式

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import parsers from '../Sub-Store/backend/src/core/proxy-utils/parsers/index.js'
-import produce from '../Sub-Store/backend/src/core/proxy-utils/producers/index.js'
-
-export default {
-    parsers,
-    produce
-}
+export * as parsers from  '../Sub-Store/backend/src/core/proxy-utils/parsers/index.js'
+export * as produce from '../Sub-Store/backend/src/core/proxy-utils/producers/index.js'
