d06f0662bf0a0de009cca211196a795d4c8680b7
tbxark <tbxark@outlook.com>
2025-10-15 09:33:07 +0800

    chore: 重构 esbuild 配置，添加 createPackageJson 插件以生成构建后的 package.json

--- packages/core/esbuild.config.js
+++ packages/core/esbuild.config.js
@@ -4,6 +4,25 @@ import process from 'node:process';
 
 const dependenciesLoader = (path) => Object.keys(JSON.parse(fs.readFileSync(path).toString()).dependencies)
 
+const createPackageJsonPlugin = () => ({
+  name: 'create-package-json',
+  setup(build) {
+    build.onEnd(() => {
+      const packageJson = {
+        ...(JSON.parse(fs.readFileSync('./package.json').toString())),
+        "name": "sub-store-convert",
+        "module": "./index.js",
+        "main": "./index.js",
+        "files": [
+          "index.js",
+        ]
+      }
+      delete packageJson.scripts
+      fs.writeFileSync('./build/package.json', JSON.stringify(packageJson, null, 2))
+    })
+  }
+})
+
 esbuild.build({
   entryPoints: ['src/index.js'],
   target: 'esnext',
@@ -19,16 +38,6 @@ esbuild.build({
   alias: {
     '@/core/app': './src/core/app'
   },
+  plugins: [createPackageJsonPlugin()],
 }).catch(() => process.exit(1))
 
-const packageJson = {
-  ...(JSON.parse(fs.readFileSync('./package.json').toString())),
-  "name": "sub-store-convert",
-  "module": "./index.js",
-  "main": "./index.js",
-  "files": [
-    "index.js",
-  ]
-}
-delete packageJson.scripts
-fs.writeFileSync('./build/package.json', JSON.stringify(packageJson, null, 2))
